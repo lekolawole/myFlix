a8181266d46fac24e2d3afe65a294249e91749de
lekolawole <lekolawole@gmail.com>
2022-04-01 18:55:57 -0400

    Corrected syntax in server.js

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 console.log('Hello Node!');
 
 console.log('Goodbye.');
+
+import { Buffer } from 'buffer';
+
+ const bufs = Buffer.from([1, 2, 3, 4]);
+
+ for (const buf of bufs) {
+   console.log(buf);
+ }
