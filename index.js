f1f88019b7cfe60fb35dddab5548131ea9419dfe
lekolawole <lekolawole@gmail.com>
2022-04-20 19:47:13 -0400

    Updated connection URI & config vars

--- index.js
+++ index.js
@@ -33,9 +33,12 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+/*mongoose.connect('mongodb://localhost:27017/myFlixDB', {
     useNewUrlParser: true, useUnifiedTopology: true 
-}); 
+});*/
+mongoose.connect('process.env.CONNECTION_URI', {
+    useNewUrlParser: true, useUnifiedTopology: true 
+});
 
 //Using Morgan, BodyParser, UUID to log requests
 app.use(morgan('common'));
