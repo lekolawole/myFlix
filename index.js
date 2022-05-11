034eb4752f41df5d8e9f75553f4d739af92d1821
lekolawole <lekolawole@gmail.com>
2022-05-11 18:51:27 -0400

    Restored user authentication

--- index.js
+++ index.js
@@ -99,26 +99,26 @@ app.post('/users', (req, res) => {
  
 
 //READ - returns ALL movies
-// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     Movies.find()
-//     .then((movies) => {
-//         res.status(201).json(movies);
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//         res.status(500).send('Error: ' + err);
-//     });
-// });
-app.get("/movies", function (req, res) {
-  Movies.find()
-    .then(function (movies) {
-      res.status(201).json(movies);
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
     })
-    .catch(function (error) {
-      console.error(error);
-      res.status(500).send("Error: " + error);
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
     });
 });
+// app.get("/movies", function (req, res) {
+//   Movies.find()
+//     .then(function (movies) {
+//       res.status(201).json(movies);
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//       res.status(500).send("Error: " + error);
+//     });
+// });
 
 
 //READ - GET movie by Title
