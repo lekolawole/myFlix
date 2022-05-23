c9655148a9d6c370a332efb48c127101a17b468e
lekolawole <lekolawole@gmail.com>
2022-05-23 11:01:35 -0400

    Reverted changes

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 
 // UPDATE or CREATE -  add movie title to user's favorite movies using Mongoose - 
 //Expects Username and MovieID
-app.push('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, {
         $push: {FavoriteMovies: req.params.MovieID}
     }, 
